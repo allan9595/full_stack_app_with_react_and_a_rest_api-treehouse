21260f95a631a69502299f8c2cc87098401c6467
*allan9595* <bohanzhang@my.unt.edu>
2019-08-16 00:47:18 -0400

    add markdown, add buttons in coruse details

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,7 @@
 import React , { Component } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
+const ReactMarkdown = require('react-markdown');
 
 class CourseDetail extends Component {
     state = {
@@ -7,7 +9,6 @@ class CourseDetail extends Component {
     }
 
     componentDidMount(){
-        
         const id = this.props.match.params.id;
         if(id !== 'create'){
             axios.get(`http://localhost:5000/api/courses/${id}`)
@@ -21,20 +22,41 @@ class CourseDetail extends Component {
         }
     }
 
+    deleteCourse = () => {
+
+    }
     render(){
         if (!this.state.course.User) {
             return null;
         } //if the data hasn't been async into state yet, return null to prevent error throwing
+        const id = this.props.match.params.id;
         return(
+        
         <div className="bounds course--detail">
             <div className="grid-66">
                 <div className="course--header">
+                <div className="actions--bar">
+                    <div className="bounds">
+                        <div className="grid-100">
+                            <span>
+                                <Link to={`/courses/${id}/update`} className="button a1" >
+                                    Update Course
+                                </Link>
+                                
+                                {this.deleteCourse()}
+                                <Link to={`/`} className="button button-secondary a1" >
+                                    Return to List
+                                </Link>
+                            </span>
+                        </div>
+                        </div>
+                    </div>
                     <h4 className="course--label">Course</h4>
                     <h3 className="course--title">{this.state.course.title}</h3>
                     <p>By {this.state.course.User.firstName} {this.state.course.User.lastName}</p>
                 </div>
                 <div className="course--description">
-                    <p>{this.state.course.description}</p>
+                    <ReactMarkdown source={this.state.course.description} />
                 </div>
             </div>
             <div className="grid-25 grid-right">
@@ -47,7 +69,9 @@ class CourseDetail extends Component {
                     <li className="course--stats--list--item">
                     <h4>Materials Needed</h4>
                     <ul>
-                        <li>{this.state.course.materialsNeeded}</li>
+                        <li>
+                            <ReactMarkdown source={this.state.course.materialsNeeded} />
+                        </li>
                     </ul>
                     </li>
                 </ul>
